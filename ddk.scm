6600259268fdd0de7b9207658140bb4fd0f6dc41
Xiaohu Wang <xiaohu.wang0921@gmail.com>
2025-12-05 15:45:09 +0100

    OpenDisjFor

--- ddk.scm
+++ ddk.scm
@@ -10,3 +10,19 @@
 (add-totality "ddk")
 (add-eqp "ddk")
 (add-eqpnc "ddk")
+
+(add-var-name "opn" (py "rat=>rat=>rat yprod rat"))
+(add-var-name "loc" (py "rat=>rat=>boole"))
+
+(add-program-constant "Inh" (py "(rat=>rat=>boole)=>rat=>rat=>rat=>boole"))
+(add-computation-rules
+ "Inh loc a b c" "a<b andb b<c andb[if(loc a b)(negb(loc a c))(loc c b)]")
+
+(add-program-constant "OpenFor" (py "(rat=>rat=>rat yprod rat)=>(rat=>rat=>boole)=>rat=>rat=>boole"))
+(add-computation-rules
+ "OpenFor opn loc a b"
+ "a<b impb lft(opn a b)<rht(opn a b)andb[if(loc a b)(loc(lft(opn a b))(rht(opn a b)))(negb(loc(lft(opn a b)(rht(opn a b)))))]")
+
+(add-program-constant "DisjFor" (py "(rat=>rat=>boole)=>rat=>rat=>rat=>boole"))
+(add-computation-rules
+ "DisjFor loc a b c" "a<b impb b<c impb negb(loc a b)orb(loc b c)")
